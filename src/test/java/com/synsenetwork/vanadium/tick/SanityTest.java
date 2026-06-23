49ad6e33ff222c57cdbeaad24c3ffc9b8137563a
Kristián Kekeš <gamerix2006@gmail.com>
2026-06-24 01:05:54 +0200

    test: add JUnit 5 infrastructure

---
+++ src/test/java/com/synsenetwork/vanadium/tick/SanityTest.java
@@ -0,0 +1,10 @@
+package com.synsenetwork.vanadium.tick;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+
+class SanityTest {
+    @Test void junitRuns() {
+        assertEquals(4, 2 + 2);
+    }
+}
